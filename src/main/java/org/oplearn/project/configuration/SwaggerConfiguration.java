285077c7ef48c7e2af978609edf85caf10b5c84d
Nguyen Anh Tu <nguyen.anh.tu.dev@gmail.com>
2026-03-27 20:55:44 +0700

    fix swagger

--- src/main/java/org/oplearn/project/configuration/SwaggerConfiguration.java
+++ src/main/java/org/oplearn/project/configuration/SwaggerConfiguration.java
@@ -5,27 +5,33 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
 
-  @Bean
-  public OpenAPI customizeOpenAPI() {
-    final String jwtSecuritySchemeName = "bearerAuth";
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String jwtSecuritySchemeName = "bearerAuth";
 
-    return new OpenAPI()
-          .info(new Info().title("My API").version("v1"))
-          .addSecurityItem(new SecurityRequirement().addList(jwtSecuritySchemeName))
-          .components(new Components()
-                .addSecuritySchemes(jwtSecuritySchemeName,
-                      new SecurityScheme()
-                            .type(SecurityScheme.Type.HTTP)
-                            .scheme("bearer")
-                            .bearerFormat("JWT")
-                            .description("JWT Authentication")
-                )
-          );
-  }
+        return new OpenAPI()
+                .info(new Info().title("My API").version("v1"))
+
+                .servers(List.of(new Server().url("/")))
+
+                .addSecurityItem(new SecurityRequirement().addList(jwtSecuritySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(jwtSecuritySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT Authentication")
+                        )
+                );
+    }
 }
