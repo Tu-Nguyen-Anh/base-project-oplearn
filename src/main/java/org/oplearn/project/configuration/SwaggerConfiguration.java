d0bb66936c6ea630298954add4f27b056ea88f31
Nguyen Anh Tu <nguyen.anh.tu.dev@gmail.com>
2026-03-27 21:03:08 +0700

    refactor swagger

--- src/main/java/org/oplearn/project/configuration/SwaggerConfiguration.java
+++ src/main/java/org/oplearn/project/configuration/SwaggerConfiguration.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,15 +15,27 @@ import java.util.List;
 @Configuration
 public class SwaggerConfiguration {
 
+    @Value("${server.port:8188}")
+    private String serverPort;
+
     @Bean
     public OpenAPI customizeOpenAPI() {
         final String jwtSecuritySchemeName = "bearerAuth";
 
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:" + serverPort);
+        localServer.setDescription("Server chạy tại Local");
+
+        Server prodServer = new Server();
+        prodServer.setUrl("https://api.codedeohayduoc.io.vn");
+        prodServer.setDescription("Server thực tế (Production)");
+
         return new OpenAPI()
-                .info(new Info().title("My API").version("v1"))
-
-                .servers(List.of(new Server().url("/")))
-
+                .info(new Info()
+                        .title("My API - SOAR Project")
+                        .version("v1")
+                        .description("Tài liệu API cho dự án News"))
+                .servers(List.of(localServer, prodServer))
                 .addSecurityItem(new SecurityRequirement().addList(jwtSecuritySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(jwtSecuritySchemeName,
@@ -30,7 +43,7 @@ public class SwaggerConfiguration {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")
-                                        .description("JWT Authentication")
+                                        .description("Nhập Access Token để gọi API")
                         )
                 );
     }
